f21269eb0545088895b615186bbe4396eae0f1f6
Leo Ledlow <31348319+CascaTheGreat@users.noreply.github.com>
2024-02-06 21:28:03 -0500

    update styling

--- src/components/MapboxComponent.js
+++ src/components/MapboxComponent.js
@@ -25,29 +25,40 @@ function MapboxComponent() {
       getTargetPosition: (d) => d.target,
       getSourceColor: [255, 165, 0],
       getTargetColor: [0, 191, 255],
-      getWidth: 1,
+      getWidth: 3,
     }),
   ];
 
   return (
-    <div className="map-container">
-      <DeckGL
-        initialViewState={INITIAL_VIEW_STATE}
-        controller={true}
-        layers={layers}
-        getTooltip={({ object }) =>
-          object && `From: ${object.flyFrom}, To: ${object.flyTo}`
-        }
-        style={{ width: "100%", height: "99%" }}
-      >
-        <Map
-          mapboxAccessToken={
-            "pk.eyJ1IjoibGxlZGxvdzIyIiwiYSI6ImNsbmM5anpxYTA0OGwybW9udjQ1a3RhN2kifQ.4eYuK2hnBiMavi51EtheHQ"
+    <div className="map-page">
+      <div className="map-container">
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          layers={layers}
+          getTooltip={({ object }) =>
+            object && `From: ${object.flyFrom}, To: ${object.flyTo}`
           }
-          mapStyle="mapbox://styles/mapbox/dark-v9"
-          style={{ width: "100%", height: "100%" }}
-        />
-      </DeckGL>
+          style={{
+            width: "95%",
+            height: "95%",
+            left: "auto",
+            overflow: "hidden",
+            borderRadius: "10px",
+          }}
+        >
+          <Map
+            attributionControl={false}
+            initialViewState={INITIAL_VIEW_STATE}
+            mapboxAccessToken={
+              "pk.eyJ1IjoibGxlZGxvdzIyIiwiYSI6ImNsbmM5anpxYTA0OGwybW9udjQ1a3RhN2kifQ.4eYuK2hnBiMavi51EtheHQ"
+            }
+            mapStyle="mapbox://styles/mapbox/dark-v9"
+            style={{ width: "100%", height: "100%" }}
+            preventStyleDiffing={true}
+          />
+        </DeckGL>
+      </div>
     </div>
   );
 }
